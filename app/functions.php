5d3b2fd710028f07b232c992e7a2fa4b44c6557a
Seriusman Waruwu <seriusman0@gmail.com>
2021-10-31 19:46:36 +0700

    replace img

--- app/functions.php
+++ app/functions.php
@@ -114,13 +114,58 @@ function updateProduct($data)
             WHERE product_id = '$_GET[id]'";
         mysqli_query($conn, $query);
     } else {
-        $gambar = upload();
+        $nameBefore = mysqli_fetch_assoc(getItem('product', $_GET['id']))['product_img'];
+        $gambar = uploadUpdate($nameBefore);
         $query  = "UPDATE product
 			SET product_img = '$gambar', product_name =  '$product_name', product_desc = '$product_desc', product_category = '$product_category', product_stock = '$product_stock', product_price = '$product_price', update_at = current_timestamp
             WHERE product_id = '$_GET[id]'";
         mysqli_query($conn, $query);
     }
     return (mysqli_affected_rows($conn));
+}
+
+function uploadUpdate($nameBefore)
+{
+
+    $namaFile = $_FILES['gambar']['name'];
+    $ukuranFile = $_FILES['gambar']['size'];
+    $error = $_FILES['gambar']['error'];
+    $tmpName = $_FILES['gambar']['tmp_name'];
+
+    // cek apakah gambar di upload
+    if ($error === 4) {
+        echo "<script>
+				alert ('Pilih Gambar terlebih dahulu');
+			</script>";
+        return 'false';
+    }
+
+    //cek apakah yang di upload adalah gambar
+    $ekstensiGambarValid = ['jpg', 'jpeg', 'png'];
+    $ekstensiGambar = explode('.', $namaFile);
+    $ekstensiGambar = strtolower(end($ekstensiGambar));
+    if (!in_array($ekstensiGambar, $ekstensiGambarValid)) {
+        echo "<script>
+				alert ('Yang Anda Upload Bukan Gambar');
+			</script>";
+        return false;
+    }
+
+    //cek jika ukuran terlalu besar 
+    if ($ukuranFile > 30000000) {
+        echo "<script>
+				alert ('Ukuran Gambar telalu Besar');
+			</script>";
+        return false;
+    }
+
+    //lolos pengecekan
+    //generate nama file baru
+    // $namaFileBaru = uniqid();
+    // $namaFileBaru .= '.';
+    // $namaFileBaru .= $ekstensiGambar;
+    move_uploaded_file($tmpName, 'img/product/' . $nameBefore);
+    return $nameBefore;
 }
 
 function upload()
