ae783e92cbce652114d9508579873afb0d0f5019
Seriusman Waruwu <seriusman0@gmail.com>
2021-10-31 19:25:35 +0700

    left sidebar

--- app/left_sidebar.php
+++ app/left_sidebar.php
@@ -6,7 +6,7 @@
                 if ($data1 == $data2) return "active";
             }
             ?>
-            <li class="navigation">Navigation</li>
+            <li class="navigation">Navigation </li>
             <li><a href="index.php" class="waves-effect waves-default <?php if (!isset($_GET['page'])) echo "active";  ?>"><i class="material-icons left-icon">dashboard</i>Dashboard</a></li>
             <li><a href="index.php?page=user" class="waves-effect waves-default <?= active($_GET['page'], 'user')  ?>"><i class="material-icons left-icon">people</i>User</a></li>
             <li><a href="index.php?page=product" class="waves-effect waves-default <?= active($_GET['page'], 'product')  ?>"><i class="material-icons left-icon">devices</i>Product</a></li>
